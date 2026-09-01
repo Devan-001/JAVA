233c45ecda9212f2b90f833377aef7dde39f163b
Deva Nandanan S <devanandanans1234@gmail.com>
2026-09-01 11:02:29 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,38 +1,14 @@
-// Main class
+class Students{
+    String name = "Anjana";
+
+    public String toString(){
+        return "Student Name: " + name;
+    }
+}
 public class Demo {
-
-    public static void main(String[] args) {
-
-        Car c = new Car();
-        Bike b = new Bike();
-        Bus bs = new Bus();
-
-        c.display();
-        b.display();
-        bs.display();
-    }
-}
-
-// First class
-class Car {
-
-    void display() {
-        System.out.println("Car object created");
-    }
-}
-
-// Second class
-class Bike {
-
-    void display() {
-        System.out.println("Bike object created");
-    }
-}
-
-// Third class
-class Bus {
-
-    void display() {
-        System.out.println("Bus object created");
+    public static void main(String[] args){
+        Students s = new Students();
+        System.out.println(s.getClass());
+        System.out.println(s.getClass().getName());
     }
 }
